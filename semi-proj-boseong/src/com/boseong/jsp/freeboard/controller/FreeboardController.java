d6c1362a92e72fa20fd92c2ce55aa25dd6fc2370
parsifal295 <parsifal295@gmail.com>
2023-09-27 15:50:20 +0900

    Update FreeboardController.java

--- semi-proj-boseong/src/com/boseong/jsp/freeboard/controller/FreeboardController.java
+++ semi-proj-boseong/src/com/boseong/jsp/freeboard/controller/FreeboardController.java
@@ -31,10 +31,13 @@ public class FreeboardController {
     boardLimit = 10;
     maxPage = (int) Math.ceil((double) listCount / boardLimit);
     startPage = (currentPage - 1) * pageLimit * pageLimit + 1;
+    endPage = startPage + pageLimit - 1;
     if (endPage > maxPage) {
       endPage = maxPage;
     }
-    PageInfo pi = new PageInfo();
+    PageInfo pi =
+        new PageInfo(listCount, currentPage, pageLimit, boardLimit, maxPage, startPage, endPage);
+
     ArrayList<Freeboard> list = new FreeboardService().selectFboardList(pi);
     String returnMe = "/views/freeboard/fboardListView.jsp";
     return returnMe;
