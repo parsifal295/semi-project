ecb8fc30f2f988c2f788ceebfda28cd9edcee920
parsifal295 <parsifal295@gmail.com>
2023-09-27 12:04:25 +0900

    자유게시판 작업중
    자유게시판 작업중자유게시판 작업중자유게시판 작업중자유게시판 작업중자유게시판 작업중자유게시판 작업중자유게시판 작업중자유게시판 작업중자유게시판 작업중자유게시판 작업중자유게시판 작업중자유게시판 작업중자유게시판 작업중자유게시판 작업중자유게시판 작업중자유게시판 작업중자유게시판 작업중자유게시판 작업중자유게시판 작업중자유게시판 작업중자유게시판 작업중자유게시판 작업중자유게시판 작업중자유게시판 작업중자유게시판 작업중

--- semi-proj-boseong/src/com/boseong/jsp/freeboard/model/dao/FreeboardDao.java
+++ semi-proj-boseong/src/com/boseong/jsp/freeboard/model/dao/FreeboardDao.java
@@ -1,5 +1,35 @@
 package com.boseong.jsp.freeboard.model.dao;
 
+import com.boseong.jsp.freeboard.model.vo.Freeboard;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Properties;
+
 public class FreeboardDao {
 
+  private Properties prop = new Properties();
+
+  public FreeboardDao() {
+    String filePath = Freeboard.class.getResource("/sql/freeboard/freeboard-mapper.xml").getPath();
+    try {
+      prop.loadFromXML(new FileInputStream(filePath));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public ArrayList<Freeboard> selectFboardList(Connection conn) {
+    ArrayList<Freeboard> list = new ArrayList<>();
+    String sql = prop.getProperty("selectFboardList");
+    try (PreparedStatement ps = conn.prepareStatement(sql)) {
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return list;
+  }
 }
---
+++ semi-proj-boseong/src/com/boseong/jsp/freeboard/model/vo/PageInfo.java
@@ -0,0 +1,113 @@
+package com.boseong.jsp.freeboard.model.vo;
+
+public abstract class PageInfo {
+  private int listCount; // 현재 일반게시글의 게시글 총 개수 => DB에서 총 게시글 개수 질의 count(*) / status='Y'
+  private int currentPage; // 현재 페이지 => request.getParameter("cpage")
+  private int pageLimit; // 한 페이지에 보여질 게시글 최대 갯수 => 10개로 고정
+  private int boardLimit; // 한 페이지에 보여질 게시글의 최대 개수 => 10개로 고정
+  private int maxPage; // 총 페이지수
+  private int startPage; // 시작 페이지
+  private int endPage; // 끝 페이지
+
+  public PageInfo() {}
+
+  public PageInfo(
+      int listCount,
+      int currentPage,
+      int pageLimit,
+      int boardLimit,
+      int maxPage,
+      int startPage,
+      int endPage) {
+    this.listCount = listCount;
+    this.currentPage = currentPage;
+    this.pageLimit = pageLimit;
+    this.boardLimit = boardLimit;
+    this.maxPage = maxPage;
+    this.startPage = startPage;
+    this.endPage = endPage;
+  }
+
+  public int getListCount() {
+    return this.listCount;
+  }
+
+  public void setListCount(int listCount) {
+    this.listCount = listCount;
+  }
+
+  public int getCurrentPage() {
+    return this.currentPage;
+  }
+
+  public void setCurrentPage(int currentPage) {
+    this.currentPage = currentPage;
+  }
+
+  public int getPageLimit() {
+    return this.pageLimit;
+  }
+
+  public void setPageLimit(int pageLimit) {
+    this.pageLimit = pageLimit;
+  }
+
+  public int getBoardLimit() {
+    return this.boardLimit;
+  }
+
+  public void setBoardLimit(int boardLimit) {
+    this.boardLimit = boardLimit;
+  }
+
+  public int getMaxPage() {
+    return this.maxPage;
+  }
+
+  public void setMaxPage(int maxPage) {
+    this.maxPage = maxPage;
+  }
+
+  public int getStartPage() {
+    return this.startPage;
+  }
+
+  public void setStartPage(int startPage) {
+    this.startPage = startPage;
+  }
+
+  public int getEndPage() {
+    return this.endPage;
+  }
+
+  public void setEndPage(int endPage) {
+    this.endPage = endPage;
+  }
+
+  @Override
+  public String toString() {
+    return "{"
+        + " listCount='"
+        + getListCount()
+        + "'"
+        + ", currentPage='"
+        + getCurrentPage()
+        + "'"
+        + ", pageLimit='"
+        + getPageLimit()
+        + "'"
+        + ", boardLimit='"
+        + getBoardLimit()
+        + "'"
+        + ", maxPage='"
+        + getMaxPage()
+        + "'"
+        + ", startPage='"
+        + getStartPage()
+        + "'"
+        + ", endPage='"
+        + getEndPage()
+        + "'"
+        + "}";
+  }
+}
