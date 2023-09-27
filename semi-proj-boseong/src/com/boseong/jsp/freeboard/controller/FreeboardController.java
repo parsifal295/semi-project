10fa25fe8d1823dfdd4e71888cb2586e3acd8ea1
parsifal295 <parsifal295@gmail.com>
2023-09-27 10:27:55 +0900

    fboardlistview

---
+++ semi-proj-boseong/src/com/boseong/jsp/freeboard/controller/FreeboardController.java
@@ -0,0 +1,16 @@
+package com.boseong.jsp.freeboard.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FreeboardController {
+
+  public String requestFreeboard(HttpServletRequest request, HttpServletResponse response) {
+
+    return "";
+  }
+}
