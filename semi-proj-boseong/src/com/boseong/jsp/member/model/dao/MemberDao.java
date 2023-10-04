ccc5e1115348f2be05a3db15fa5bc50464a59f83
hyominnnnn <dlalsgy23@gmail.com>
2023-10-04 15:10:30 +0900

    Update MemberDao.java

--- semi-proj-boseong/src/com/boseong/jsp/member/model/dao/MemberDao.java
+++ semi-proj-boseong/src/com/boseong/jsp/member/model/dao/MemberDao.java
@@ -5,5 +5,10 @@ import java.util.Properties;
 public class MemberDao {
 	
 	private Properties prop = new Properties();
+	
+	public MemberDao() {
+		
+		String file = MemberDao.class.getResource("/sql/member/member-mapper.xml").getPath();
+	}
 
 }
