6d839e80016e808d2d390b0c11f67eede27d82b0
CamoPepper <137665054+CamoPepper@users.noreply.github.com>
2023-10-04 09:53:53 +0900

    transList 연결(hs)

---
+++ semi-proj-boseong/WebContent/views/info/transportation/toBus.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>시외버스</title>
+</head>
+<body>
+	<h1>보성으로 가는 시외버스 이용 안내</h1>
+
+</body>
+</html>
---
+++ semi-proj-boseong/src/com/boseong/jsp/info/controller/TransDetailController.java
@@ -0,0 +1,43 @@
+package com.boseong.jsp.info.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TransDetailController
+ */
+@WebServlet("/detail.tr")
+public class TransDetailController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TransDetailController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String type = request.getParameter("type");
+		String page = "views/info/transportation/"+type+".jsp";
+		System.out.println(page);
+		request.getRequestDispatcher(page).forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
