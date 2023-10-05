75e17b8f4e17c2424d305542b4fb6743b8632b8e
parsifal295 <parsifal295@gmail.com>
2023-10-05 14:38:25 +0900

    ASDFASDF

--- semi-proj-boseong/src/com/boseong/jsp/member/controller/LoginController.java
+++ semi-proj-boseong/src/com/boseong/jsp/member/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.boseong.jsp.member.controller;
 
+import com.boseong.jsp.member.model.service.MemberService;
+import com.boseong.jsp.member.model.vo.Member;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,51 +9,44 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.boseong.jsp.member.model.service.MemberService;
-import com.boseong.jsp.member.model.vo.Member;
-
-/**
- * Servlet implementation class LoginController
- */
+/** Servlet implementation class LoginController */
 @WebServlet("/login.me")
 public class LoginController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+  private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+  /**
+   * @see HttpServlet#HttpServlet()
+   */
+  public LoginController() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
 
-		request.setCharacterEncoding("UTF-8");
-		
-		//System.out.println(request.getParameter("memId"));
-		//System.out.println(request.getParameter("memPwd"));
-		
-		String memId = request.getParameter("memId");
-		String memPwd = request.getParameter("memPwd");
-		
-		Member loginUser = new MemberService().loginMember(memId, memPwd);
-		
-		//System.out.println(loginUser);
-	
-	
-	
-	}
+  /**
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+    request.setCharacterEncoding("UTF-8");
 
+    // System.out.println(request.getParameter("memId"));
+    // System.out.println(request.getParameter("memPwd"));
+
+    String memId = request.getParameter("memId");
+    String memPwd = request.getParameter("memPwd");
+
+    Member loginUser = new MemberService().loginMember(memId, memPwd);
+
+    System.out.println(loginUser);
+  }
+
+  /**
+   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    doGet(request, response);
+  }
 }
