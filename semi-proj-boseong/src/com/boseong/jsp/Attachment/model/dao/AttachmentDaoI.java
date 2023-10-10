64b993f41cd4d4d4ac418fee34583aaf5e219255
parsifal295 <parsifal295@gmail.com>
2023-10-10 09:53:40 +0900

    자유게시판 삭제기능 추가중

--- semi-proj-boseong/src/com/boseong/jsp/Attachment/model/dao/AttachmentDaoI.java
+++ semi-proj-boseong/src/com/boseong/jsp/Attachment/model/dao/AttachmentDaoI.java
@@ -8,4 +8,8 @@ public interface AttachmentDaoI {
   public int insertAttachment(Connection conn, Attachment att);
 
   public Attachment selectAttachment(Connection conn, int boardNo);
+
+  public int updateAttachment(Connection conn, Attachment att);
+
+  public int deleteAttachment(Connection conn, int boardNo);
 }
