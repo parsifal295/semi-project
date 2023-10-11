abc56a8dbf4b2ff8a1694590a6d0e73cb2009277
parsifal295 <parsifal295@gmail.com>
2023-10-11 14:41:17 +0900

    자유게시판 댓글기능 추가중

---
+++ semi-proj-boseong/src/com/boseong/jsp/freeboard/controller/AjaxReplyListController.java
@@ -0,0 +1,49 @@
+package com.boseong.jsp.freeboard.controller;
+
+import com.boseong.jsp.freeboard.model.service.FreeboardService;
+import com.boseong.jsp.freeboard.model.vo.FreeboardReply;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Servlet implementation class AjaxReplyListController */
+@WebServlet("/replylist.fb")
+public class AjaxReplyListController extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * @see HttpServlet#HttpServlet()
+   */
+  public AjaxReplyListController() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    // AJAX 처리
+    int boardNo = Integer.parseInt(request.getParameter("bnoo"));
+    ArrayList<FreeboardReply> list = new FreeboardService().selectReplyList(boardNo);
+    response.setContentType("application/json; charset=UTF-8");
+    new Gson().toJson(list, response.getWriter());
+    //	    response.setContentType("text/html; charset=UTF-8");
+    //	    response.getWriter().print(123);
+  }
+
+  /**
+   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    doGet(request, response);
+  }
+}
