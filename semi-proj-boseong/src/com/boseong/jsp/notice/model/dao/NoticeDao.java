d350349bbc1b839cafb63ef428e8e0bdacf1650c
parsifal295 <parsifal295@gmail.com>
2023-10-16 09:34:19 +0900

    공지사항 게시판 작업중

--- semi-proj-boseong/src/com/boseong/jsp/notice/model/dao/NoticeDao.java
+++ semi-proj-boseong/src/com/boseong/jsp/notice/model/dao/NoticeDao.java
@@ -6,6 +6,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Properties;
@@ -26,9 +27,21 @@ public class NoticeDao {
     ArrayList<Notice> list = new ArrayList<Notice>();
     String sql = prop.getProperty("selectNoticeList");
     try (PreparedStatement ps = conn.prepareStatement(sql)) {
-
+      try (ResultSet rs = ps.executeQuery()) {
+        while (rs.next()) {
+          Notice n = new Notice();
+          n.setNoticeNo(rs.getInt("NOTICE_NO"));
+          n.setCategory(rs.getInt("CATEGORY_NO"));
+          n.setNoticeTitle(rs.getString("NOTICE_TITLE"));
+          n.setNoticeContent(rs.getString("NOTICE_CONTENT"));
+          n.setCreateDate(rs.getString("A"));
+          n.setModifyDate(rs.getString("B"));
+          n.setCount(rs.getInt("COUNT"));
+        }
+      }
     } catch (SQLException e) {
       e.printStackTrace();
     }
+    return list;
   }
 }
