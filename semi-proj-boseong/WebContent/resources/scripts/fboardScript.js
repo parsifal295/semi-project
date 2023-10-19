5654edc02aa66fba24c7cecfa5a84f8aeefe906e
parsifal295 <parsifal295@gmail.com>
2023-10-19 11:13:27 +0900

    scrap 작업중

--- semi-proj-boseong/WebContent/resources/scripts/fboardScript.js
+++ semi-proj-boseong/WebContent/resources/scripts/fboardScript.js
@@ -1,117 +1,115 @@
-<script>
-  function passwordCheck() {
+function passwordCheck() {
   var temp = <%= fb.getPassword() %>;
   var pw1 = $("#passwd").val();
   var pw2 = $("#pass").val();
-
   if (pw1 == temp) {
     // 암호 일치시 : 수정 페이지로 가기
     $("#checker").submit(function (e) {
       $(this).attr("action", "<%=contextPath %>/updateForm.fb");
     })
   }
-
   if (pw2 == temp) {
     $("#chkr").submit(function (f) {
       $(this).attr("action", "<%=contextPath %>/deleteForm.fb");
     })
   }
 };
-  function checkDelete() {
+
+function checkDelete() {
   var rNum = $('#reply-number').attr("value");
   console.log(rNum);
   var usrPass = $('#pass-delete').val();
   var boardNo = <%=fb.getBoardNo() %>;
   $.ajax({
-    url : 'deleteReply.fb',
-  data : {
-    bno : boardNo,
-  rNum : rNum,
-  usrPass : usrPass
+    url: 'deleteReply.fb',
+    data: {
+      bno: boardNo,
+      rNum: rNum,
+      usrPass: usrPass
     },
-  success : function (result) {
+    success: function (result) {
       if (result == 1) {
-    alert('댓글 삭제 성공');
+        alert('댓글 삭제 성공');
       } else {
-    alert('댓글 삭제 실패');
+        alert('댓글 삭제 실패');
       }
     },
   })
   location.href = "<%=contextPath%>/detailView.fb?bno=" + boardNo;
 };
-  function insertReply() {
-    $.ajax({
-      url: 'replyinsert.fb',
-      type: 'POST',
-      data: {
-        bno: <%= fb.getBoardNo() %>,
+function insertReply() {
+  $.ajax({
+    url: 'replyinsert.fb',
+    type: 'POST',
+    data: {
+      bno: <%= fb.getBoardNo() %>,
       id: $('#replyId').val(),
       pw: $('#replyPw').val(),
       content: $('#replyContent').val(),
       ip: $('#displayIp').val()
     },
-      success : function (result) {
+    success: function (result) {
       if (result > 0) {
-    $('#replyId').val('');
-  $('#replyPw').val('');
-  $('#replyContent').val('');
-  selectReplyList();
+        $('#replyId').val('');
+        $('#replyPw').val('');
+        $('#replyContent').val('');
+        selectReplyList();
       }
     }
   });
   location.reload();
 };
-  function selectReplyList() {
-    $.ajax({
-      url: 'replylist.fb',
-      data: { bno: <%= fb.getBoardNo() %>},
-      success : function(result) {
-    // 댓글 개수만큼 루프반복 (댓글전체출력을 위해서..)
-    let resultStr = '';
-  for (let i in result) {
-    resultStr += '<tr>'
-    + '<td width="200">' + result[i].writer + "(" + result[i].ipAddress + ")" + '</td>'
-    + '<td width="300" align="center">' + result[i].createDate + '</td>'
-    + '<td width="100" align="right">'
-    + '<a href = "#" onclick="showEditReply(this);" data-toggle="modal" data-target="#editCommentModal" value="'
-    + result[i].replyNo + '">수정</a>' + ' | '
-    + '<a href = "#" data-toggle="modal" id="reply-number" data-target="#delete-comment-modal" value="'
-    + result[i].replyNo + '">삭제</a>' + '</td>'
-    + '</tr>'
-    + '<tr>'
-    + '<td colspan="3" id="comment-text' + result[i].replyNo + '">' + result[i].content + '</td>'
-    + '</tr>' + '</hr>';
+function selectReplyList() {
+  $.ajax({
+    url: 'replylist.fb',
+    data: { bno: <%= fb.getBoardNo() %>},
+    success: function (result) {
+      // 댓글 개수만큼 루프반복 (댓글전체출력을 위해서..)
+      let resultStr = '';
+      for (let i in result) {
+        resultStr += '<tr>'
+          + '<td width="200">' + result[i].writer + "(" + result[i].ipAddress + ")" + '</td>'
+          + '<td width="300" align="center">' + result[i].createDate + '</td>'
+          + '<td width="100" align="right">'
+          + '<a href = "#" onclick="showEditReply(this);" data-toggle="modal" data-target="#editCommentModal" value="'
+          + result[i].replyNo + '">수정</a>' + ' | '
+          + '<a href = "#" data-toggle="modal" id="reply-number" data-target="#delete-comment-modal" value="'
+          + result[i].replyNo + '">삭제</a>' + '</td>'
+          + '</tr>'
+          + '<tr>'
+          + '<td colspan="3" id="comment-text' + result[i].replyNo + '">' + result[i].content + '</td>'
+          + '</tr>' + '</hr>';
       }
-  $('#reply-area thead').html(resultStr);
+      $('#reply-area thead').html(resultStr);
     },
-  error : function(e, msg){
-    console.log('댓글 읽어오기 실패~');
-  console.log(msg);
-        
+    error: function (e, msg) {
+      console.log('댓글 읽어오기 실패~');
+      console.log(msg);
+
     }
   })
 };
 $(() => {
-    selectReplyList();
+  selectReplyList();
   //setInterval(selectReplyList, 1000);
 });
 
-  function showEditReply(f) {
+function showEditReply(f) {
   var rNum = $(f).attr("value");
   $.ajax({
-    url : 'replyUpdateForm.fb',
-  data : {
-    replyNo : rNum,
-  bno : <%= fb.getBoardNo() %>
+    url: 'replyUpdateForm.fb',
+    data: {
+      replyNo: rNum,
+      bno: <%= fb.getBoardNo() %>
     },
-  success : function(result) {
-    $('#reply-number').attr("value", rNum);
-  $('#replyEdit').val(result.content);
-  $('#reply-password').attr("value", result.password);
+    success: function (result) {
+      $('#reply-number').attr("value", rNum);
+      $('#replyEdit').val(result.content);
+      $('#reply-password').attr("value", result.password);
     }
   })
 };
-  function checkReplyPassword() {
+function checkReplyPassword() {
   var userPwd = $('#reply-passform').val();
   var replyPwd = $('#reply-password').val();
   var replyToUpdate = $('#replyEdit').val();
@@ -133,12 +131,12 @@ $(() => {
   } else {
     alert('비밀번호가 틀립니다.');
     $(() => {
-    location.href = "<%=contextPath%>/detailView.fb?bno=" + boardNo;
+      location.href = "<%=contextPath%>/detailView.fb?bno=" + boardNo;
     })
   }
 };
-  // GET IP ADDRESS
-  $.getJSON("https://jsonip.com/", function (data) {
+// GET IP ADDRESS
+$.getJSON("https://jsonip.com/", function (data) {
   var obj = JSON.stringify(data, null, 2);
   var temp = JSON.parse(obj, null, 2);
   console.log(obj);
@@ -146,4 +144,3 @@ $(() => {
   $("#ipvalue").val(ip);
   $("#displayIp").val(ip);
 });
-</script>
